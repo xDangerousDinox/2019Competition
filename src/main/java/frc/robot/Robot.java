4da9bcda8a31e9fc44e3099d3124f3e5c6d52606
DOGMATICSKEPTIC <ahoyt2014@gmail.com>
2019-02-21 13:20:43 -0800

    Quick clean up

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -50,7 +50,7 @@ public class Robot extends TimedRobot {
 
   public static Joystick joystick = new Joystick(0);
 
-  public static BaseCamera camera = new ImplCamera();// TODO REPLACE THIS IMMEDITELY!
+  public static BaseCamera camera = new ImplCamera();
   public static Drivetrain drivetrain = new Drivetrain();
   public static UltrasonicSensor sensor = new UltrasonicSensor();
 
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,7 +7,7 @@ public class RobotMap {
     public static final int FRONT_RIGHT_TALON = 3;
     public static final int BACK_RIGHT_TALON = 0;
 
-    public static final int FRONT_LEFT_ENCODER = 0;//TODO
+    public static final int FRONT_LEFT_ENCODER = 0;
     public static final int BACK_LEFT_ENCODER = 1;
     public static final int FRONT_RIGHT_ENCODER = 4;
     public static final int BACK_RIGHT_ENCODER = 5;
